66dba5844540d9e9da68e7d859b8032e91c3c4db
Shreyas <96154860+g-shrey@users.noreply.github.com>
2023-02-13 21:28:17 -0500

    Update users.js

--- test/users.js
+++ test/users.js
@@ -1,14 +1,9 @@
 import supertest from "supertest";
 
-//const request = supertest("https://gorest.co.in/public-api/")
 const request = supertest("https://automationintesting.online")
-
-const TOKEN = 
-    "46cc5d0cf5b76cefcae1264137564fba80774d0aa85186146d3e4aff06a0803b";
 
 describe("Users", () => {
     it("GET /users", () => {
-        //request.get('users?access-token=${TOKEN}').end((err,res) => {
         request.get('/room/').end((err,res) => {
                 console.log(err);
                 console.log(res);
